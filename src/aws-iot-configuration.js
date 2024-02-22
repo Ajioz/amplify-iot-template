33fcb1abc6d2099ea1237500e43c4043322eef07
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-02-22 17:14:50 +0100

    template push

---
+++ src/aws-iot-configuration.js
@@ -0,0 +1,7 @@
+var awsIotConfiguration = {
+    endpoint: 'wss://<ATS-endpoint>/mqtt',
+    region: '<REGION>',
+    poolId: '<Cognito-identity-pool-id>',
+    host: '<ATS-endpoint>'
+  };
+ module.exports = awsIotConfiguration;
